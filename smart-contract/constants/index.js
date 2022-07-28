606f74d557addd8158c355cd8d8cb41c5ace23d7
JQ <hi.jiaqizhang@gmail.com>
2022-07-28 17:17:17 +0100

    update address

--- smart-contract/constants/index.js
+++ smart-contract/constants/index.js
@@ -1,5 +1,5 @@
 //import fashionlistAddresses from './fashionlist.json';
-const METADATA_URL = "QmNU12GQ59rnZkdpkZHt7DoFFQv76rkGSPSYRLxWinSkaU";
+const METADATA_URL = "https://ipfs.io/ipfs/QmNU12GQ59rnZkdpkZHt7DoFFQv76rkGSPSYRLxWinSkaU/";
 const PREVIEW_URL = "https://ipfs.io/ipfs/QmPJmF7wusU7XHvneWL9jFLyFaSCgRSHEG78d35f3NazC3";
 //const FASHIONLIST = fashionlistAddresses;
 
